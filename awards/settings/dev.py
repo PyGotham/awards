12fcd447779bfc7957823f13d55ca06ee6819286
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-09-12 14:59:43 -0400

    Use Django Debug Toolbar
    This will add [django-debug-toolbar][toolbar] to the development
    environment. Adding the necessary settings results in [F405] errors from
    [Flake8] so these errors will be ignored in this particular module.
    
    [f405]: https://flake8.pycqa.org/en/latest/user/error-codes.html?highlight=F405
    [flake8]: https://flake8.pycqa.org
    [toolbar]: https://django-debug-toolbar.readthedocs.io

--- awards/settings/dev.py
+++ awards/settings/dev.py
@@ -5,3 +5,9 @@ from awards.settings.base import *  # NOQA
 SECRET_KEY = "django-insecure-@a*u0tmk#a@+3h4#sxhv2za+vm+hlei(bu685cw&t05gf6620n"
 
 DEBUG = True
+
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+
+INTERNAL_IPS = ["127.0.0.1"]
--- awards/urls.py
+++ awards/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from __future__ import annotations
 
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar  # type: ignore[import]
+
+    urlpatterns.insert(0, path("__debug__/", include(debug_toolbar.urls)))
