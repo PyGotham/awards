6ac3420a52df41881e0168b53a4b6a935490506d
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-09-12 23:46:30 -0400

    Add a base template
    This template doesn't do much, but it will provide a common starting
    point for more functional templates.

--- awards/settings/base.py
+++ awards/settings/base.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = "awards.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
